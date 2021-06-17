6c1b04d240cf5090846d1ccca0f27b9999f380e2
anaysoul <soulyana_lakew@outlook.com>
2021-06-17 18:01:12 -0400

    implemented open and close functioanlity with classes

--- script.js
+++ script.js
@@ -5,6 +5,25 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
+// open modal
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// close modal
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// open button
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+
+// close button
+btnCloseModal.addEventListener('click', closeModal);
+
+// close modal when clicked on overaly
+overlay.addEventListener('click', closeModal);
